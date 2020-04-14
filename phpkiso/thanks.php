178d7be9d5b5cf4543ca4d77ad32c85ea18b88cb
Kousei3939 <63569282+Kousei3939@users.noreply.github.com>
2020-04-14 17:16:47 +0900

    4/14

---
+++ phpkiso/thanks.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML PUBLIC"-//W3C//DTD HTML 4.01 Transitional//EN">
+ <html>
+  <head>
+   <meta http-equiv="Content-Type" content="text/html;" charset="UTF-8">
+    <title>PHP基礎</title>
+  </head>
+ <body>
+<?php
+$nickname=$_POST['nickname'];
+$email=$_POST['email'];
+$goiken=$_POST['goiken'];
+
+$nickname=htmlspecialchars("$nickname");
+$email=htmlspecialchars("$email");
+$goiken=htmlspecialchars("$goiken");
+
+print $nickname;
+print '様<br>';
+print'ご意見ありがとうございました';
+print'いただいたご意見<br>';
+print $goiken;
+print'<br>';
+print $email;
+print'にメールを送りましたのでご確認ください';
+
+$email_sub='アンケートを受け付けました';
+$email_body=$nickname."様へ¥nアンケートへのご協力ありがとうございました";
+$email_body=html_entity_decode($email_body,ENT_QUOTES,"UTF-8");
+$email_head='From:xxx@xxx.co.jp';
+mb_language('japanese');
+mb_internal_encoding("UTF-8");
+mb_send_mail($email,$mail_sub,$mail_body,$mail_head);
+
+?>
+ </body>
+ </html>
